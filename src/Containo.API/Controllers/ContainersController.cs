901f35be1d0236d61f60e63c78217e867320d984
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-08-17 11:14:11 +1000

    Add basic API

---
+++ src/Containo.API/Controllers/ContainersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Containo.API.Contracts;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Containo.API.Controllers
+{
+    [Route("api/containers")]
+    public class ContainersController : Controller
+    {
+        /// <summary>
+        ///     Gets a all the running containers
+        /// </summary>
+        /// <response code="200">Returns list of all running containers</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Container>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> Get()
+        {
+            var dockerClusterEndpoint = new Uri("npipe://./pipe/docker_engine");
+            var client = new DockerClientConfiguration(dockerClusterEndpoint).CreateClient();
+            var containersListParameters = new ContainersListParameters
+            {
+                All = true
+            };
+
+            var containers = await client.Containers.ListContainersAsync(containersListParameters);
+
+            var runningContainers = containers.Select(container => new Container
+            {
+                Id = container.ID,
+                Image = container.Image,
+                Status = container.Status,
+                Labels = container.Labels,
+                Names = container.Names,
+                State = container.State
+            }).ToList();
+
+            return Ok(runningContainers);
+        }
+    }
+}
---
+++ src/Containo.API/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Containo.API
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(swaggerUiOptions => { swaggerUiOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Containo API"); });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            var swaggerInfo = new Info
+            {
+                Title = "Containo API",
+                Version = "v1",
+                Description = "API to manage the Containo platform"
+            };
+            services.AddSwaggerGen(swaggerGenerationOptions =>
+            {
+                swaggerGenerationOptions.SwaggerDoc("v1", swaggerInfo);
+                swaggerGenerationOptions.DescribeAllEnumsAsStrings();
+                var documentationPath = GetDocumentationPath();
+                swaggerGenerationOptions.IncludeXmlComments(documentationPath);
+            });
+        }
+
+        private static string GetDocumentationPath()
+        {
+            return Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "Containo.API.xml");
+        }
+    }
+}
